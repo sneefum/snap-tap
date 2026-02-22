cb38f9686cfb390c1a338251526123b4060936a0
sneefum <3x1b.yt@gmail.com>
2026-02-22 13:20:06 +0100

    reading from /dev/input, made uinput, forwarded all keyboard events to program.

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module snap-tap
 
 go 1.25.7
+
+require github.com/bendahl/uinput v1.7.0
+
+require golang.org/x/sys v0.41.0 // indirect
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"encoding/binary"
+
+	"golang.org/x/sys/unix"
+	"github.com/bendahl/uinput"
+)
+
+type timeval struct {
+	seconds uint64
+	microseconds uint64
+}
+
+type input_event struct {
+	time timeval
+	input_type uint16
+	keycode uint16
+	value uint32
+}
+
+var EVIOCGRAB uint = 0x40044590
+
+func main() {
+	f, err := os.Open("/dev/input/event12")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	
+	keyboard, err := uinput.CreateKeyboard("/dev/uinput", []byte("testkeyboard"))
+	if err != nil {
+		return
+	}
+	
+	defer keyboard.Close()
+
+	unix.IoctlSetInt(int(f.Fd()), EVIOCGRAB, 1)
+
+	defer unix.IoctlSetInt(int(f.Fd()), EVIOCGRAB, 0)
+
+	keyboard.KeyPress(uinput.KeyA)
+
+
+	buf := make([]byte, 24)
+
+	for {
+		f.Read(buf)
+		time := timeval{
+			seconds: binary.LittleEndian.Uint64(buf[0:8]),
+			microseconds: binary.LittleEndian.Uint64(buf[8:16]),
+		}
+		input := input_event{
+			time: time,
+			input_type: binary.LittleEndian.Uint16(buf[16:18]),
+			keycode: binary.LittleEndian.Uint16(buf[18:20]),
+			value: binary.LittleEndian.Uint32(buf[20:24]),
+		}
+ 		fmt.Printf("%+v\n", input)
+	}
+}
